18c525f53f68c558f82ac0ae531af9f04136196a
hershey890 <hersh.joshi1@gmail.com>
2018-09-08 18:24:21 -0700

    GPS plotting code now works, but is very slow (may be a python thing)

--- Map_Visualization/DataMapper.py
+++ Map_Visualization/DataMapper.py
@@ -1,4 +1,7 @@
 # module DataMapper.py
+# To map a specific region, check out these links:
+# https://stackoverflow.com/questions/25428512/draw-a-map-of-a-specific-country-with-cartopy
+# https://github.com/SciTools/cartopy/issues/1048
 
 ###########################################################################
 #   DATA FORMAT - .txt file
@@ -16,19 +19,73 @@
 #   Column 12: Fluorescence
 ###########################################################################
 
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
 
 class DataMapper:
     ''' Plots all the data points for a particular attribute (ex. (lat, long, temp), (lat, long, conductivity) '''
 
-    # TODO: rember that there are data points with NaN
+    # Remember that there are data points with NaN
 
-    def __init__(self):
+    def __init__(self, file='SampleData.txt'):
+        ''' Only used for initializing member variables '''
+        self.file = file
         self.is_heading_plotted = False
         self.is_course_plotted = False
 
+        self.ax = plt.axes(projection=ccrs.Mollweide())
+        self.ax.coastlines()
+        # plt.show()
+        # TODO: implement a method of zooming into a particular region
+
+        # TODO: see if there's anything I can do to zoom into the area of interest
+
     def plot_GPS(self):
         ''' Plots only location data with no other attributes '''
-        pass
+
+        with open(self.file, 'r') as data:
+            count = 0
+
+            # Iterates through lines in the file
+            for line in data:
+                # if count > 1000:
+                #    break
+                # count += 1
+
+                num_space = 0
+                long = ''
+                lat = ''
+                error_flag = False
+
+                # Reads through a single line to save the latitude and longitude
+                for char in line:
+                    # ERROR: if the line contains NaN just skip it
+                    if char == 'N':
+                        error_flag = True
+                        break
+                    if char == ' ':
+                        num_space += 1
+                    if num_space == 3:
+                        long += char
+                    elif num_space == 4:
+                        lat += char
+                    elif num_space == 5:
+                        break
+
+                if error_flag:
+                    continue
+
+                # Remove the trailing comma
+                lat = lat[:-1]
+                long = long[:-1]
+                # Convert long and lat to floats
+                lat = float(lat)
+                long = float(long)
+
+                # Plot data - plot(x, y)
+                self.ax.plot(long, lat, 'bo', markersize=7, transform=ccrs.Geodetic())
+
+        plt.show()
 
     def plot_ship_heading(self):
         ''' Plots GPS data and ship heading in degrees as a vector on top of other data '''
---
+++ Map_Visualization/test2.py
@@ -0,0 +1,9 @@
+from DataMapper import DataMapper
+
+Map = DataMapper()
+
+Map.plot_GPS()
+
+
+# with open('SampleData.txt', 'r') as data:
+#    print('can print')
