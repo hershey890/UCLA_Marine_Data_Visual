02d0e9dad5466c71c7e953ded32cbd7e3f877e1c
hershey890 <hersh.joshi1@gmail.com>
2018-09-10 12:44:25 -0700

    Higher quality background image

--- Map_Visualization/DataMapper.py
+++ Map_Visualization/DataMapper.py
@@ -4,8 +4,11 @@
 # https://github.com/SciTools/cartopy/issues/1048
 
 # TODO: have a background img/map
-# TODO: increased resolution
 # TODO: use folium to have dynamic maps
+# TODO: make a GUI that allows for more flexible movement of the plot (this may require folium)
+# TODO: increased res on the borders
+# TODO: labels on the map
+# TODO: hq map of LA/CA region
 
 ###########################################################################
 #   DATA FORMAT - .txt file
@@ -26,7 +29,8 @@
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import cartopy.feature as cfeature
-from matplotlib.offsetbox import AnchoredText
+# from matplotlib.offsetbox import AnchoredText
+import cartopy.io.img_tiles as cimgt
 
 class DataMapper:
     ''' Plots all the data points for a particular attribute (ex. (lat, long, temp), (lat, long, conductivity) '''
@@ -39,14 +43,27 @@ class DataMapper:
         self.is_heading_plotted = False
         self.is_course_plotted = False
 
-        # Changes the display size, I'm not too sure how to adjust zoom though
-        # TODO: get the margins correct
-        plt.figure(figsize=(10, 6)) # the position of this in the code seems to matter more than it should, idk why
-        self.ax = plt.axes(projection=ccrs.PlateCarree())
+        stamen_terrain = cimgt.StamenTerrain()
+
+        # Changes the display size
+        fig = plt.figure(figsize=(10, 6))
+
+        self.ax = fig.add_subplot(1, 1, 1, projection=stamen_terrain.crs)
+
+        # Sets the margins for the plot
+        # TODO: make the margins flexible and based on the data points
+        self.ax.set_extent([-124, -113, 31, 38], crs=ccrs.Geodetic())
+
+        self.ax.add_image(stamen_terrain, 8)
+
+
+        # self.ax = plt.axes(projection=ccrs.PlateCarree())
 
     def plot_GPS(self):
         ''' Plots only location data with no other attributes '''
 
+        # READING/SAVING DATA FROM FILE
+        #################################################################
         fLong_list = []  # x coord
         fLat_list = []  # y coord
 
@@ -81,10 +98,14 @@ class DataMapper:
                 fLong_list.append(float(fLongitude[:-1]))  # x-coord
                 fLat_list.append(float(fLatitude[:-1]))  # y-coord
 
-        #########################################################################################
         # MAP FORMATTING
-        self.ax.stock_img()
-        # TODO: have a better background stock image
+        #################################################################
+        # Background stock image
+        # describes the how to add a background img, it requires a .json file http://earthpy.org/cartopy_backgroung.html
+        # self.ax.stock_img()
+        # TODO: have a better background stock
+
+        # Province borders
         states_provinces = cfeature.NaturalEarthFeature(
             category='cultural',
             name='admin_1_states_provinces_lines',
@@ -92,20 +113,18 @@ class DataMapper:
             facecolor='none')
         self.ax.add_feature(states_provinces, edgecolor='gray')
 
-        # Changes the resolution
+        # Adding coastlines
         self.ax.coastlines(resolution='10m', color='black', linewidth=1)
             # These also work but the resolution seems to be lower
             # self.ax.add_feature(cfeature.LAND)
             # self.ax.add_feature(cfeature.COASTLINE)
+
         # Plots country borders
         self.ax.add_feature(cfeature.BORDERS)
+
         # Plots all the points
-        self.ax.plot(fLong_list, fLat_list, 'bo', markersize=2, transform=ccrs.PlateCarree())
-        # Sets the margins for the plot
-        # TODO: make the margins flexible and based on the data points
-        # TODO, make a GUI that allows for more flexible movement of the plot (this may require folium)
-        # TODO: projections seem to be off
-        self.ax.set_extent([-124, -113, 31, 38], crs=ccrs.PlateCarree())
+        self.ax.plot(fLong_list, fLat_list, 'bo', markersize=2, transform=ccrs.Geodetic())
+
         plt.show() # I can prob remove this and have a display function at the end/
 
     def plot_ship_heading(self):
